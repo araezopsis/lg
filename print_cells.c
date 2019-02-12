5da994d733cd442e6cbb4f60a9606a702cb7d929
Toshihiro Arae <aroearue@gmail.com>
2019-02-12 17:33:25 +0900

    プリントを一行ずつに変更

--- print_cells.c
+++ print_cells.c
@@ -1,15 +1,16 @@
 #include "lg.h"
 
 void print_map(int row_n, int col_n, int map[row_n][col_n]) {
+    char row_string[col_n + 1];
+    row_string[col_n] = '\0';
     for(int i = 0; i < row_n; i++) {
         for(int j = 0; j < col_n; j++) {
             if(map[i][j] == 0) {
-                printw("%s ", " ");
+                row_string[j] = ' ';
             } else {
-                printw("%s ", "+");
+                row_string[j] = '+';
             }
-
         }
-        printw("\n");
+        printw("%s \n", row_string);
     }
 }
