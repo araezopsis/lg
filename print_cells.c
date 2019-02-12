282d028fb8e9e1861909452bdc56b86dce7bae5a
Toshihiro Arae <aroearue@gmail.com>
2019-02-12 09:42:57 +0900

    表示変更

--- print_cells.c
+++ print_cells.c
@@ -4,9 +4,9 @@ void print_map(int row_n, int col_n, int map[row_n][col_n]) {
     for(int i = 0; i < row_n; i++) {
         for(int j = 0; j < col_n; j++) {
             if(map[i][j] == 0) {
-                printw("%s ", "+");
-            } else {
                 printw("%s ", " ");
+            } else {
+                printw("%s ", "+");
             }
 
         }
